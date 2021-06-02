45f8bfb0ef9d86e53ca28ef433946ca8330a8317
johnb8005 <johnb8005@users.noreply.github.com>
2021-06-02 10:34:46 +0000

    deploy: a90538e63ff0c52286cb6c9b0874010a2843808d

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,3 +1,4 @@
+export const SNOWPACK_PUBLIC_GIT_SHA = "a90538e63ff0c52286cb6c9b0874010a2843808d";
 export const SNOWPACK_PUBLIC_URL = "/signature";
 export const MODE = "development";
 export const NODE_ENV = "development";
