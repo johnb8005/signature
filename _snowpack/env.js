cabb0b90fedeeb57ce5fde563d4b0fa58970d0a0
johnb8005 <johnb8005@users.noreply.github.com>
2021-06-17 13:12:14 +0000

    deploy: 456313d3177b406204b908fa23874bb0eec63c0e

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "e1b20b642e5ea4e2a62b77498da328aa91366ace";
+export const SNOWPACK_PUBLIC_GIT_SHA = "456313d3177b406204b908fa23874bb0eec63c0e";
 export const SNOWPACK_PUBLIC_URL = "/signature";
 export const MODE = "development";
 export const NODE_ENV = "development";
--- src/symmetric/index.js
+++ src/symmetric/index.js
@@ -1,9 +1,10 @@
 import React from "../../_snowpack/pkg/react.js";
 import * as U from "../crypto/symmetric/aes.js";
-const secretKey = "rbdhrbserjvcejg3rbdhrbserjvcejg3";
+const secretKey = "durbdhrbserjvcejg37fg3hcishfjkic";
 export default () => {
   U.encryptCompact("hello7!", secretKey).then((x) => {
     console.log(x);
+    const y = "18c9a475d6a8103069466e25e076b424:9e27d4764d01b8f113fd03204069030a";
     U.decryptCompact(x, secretKey).then((e) => console.log(e));
   });
   return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("h1", null, "Symmetric"), /* @__PURE__ */ React.createElement("code", null, "rawKey"), /* @__PURE__ */ React.createElement("code", null, "Message: ", "message"));
