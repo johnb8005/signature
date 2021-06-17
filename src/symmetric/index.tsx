456313d3177b406204b908fa23874bb0eec63c0e
Johan <johan@macair2020>
2021-06-17 15:11:35 +0200

    end session

--- src/symmetric/index.tsx
+++ src/symmetric/index.tsx
@@ -3,11 +3,13 @@
 import React from "react";
 import * as U from "../crypto/symmetric/aes";
 
-const secretKey = "rbdhrbserjvcejg3rbdhrbserjvcejg3"; // key size 256, string length = 256/8 = 32
+const secretKey = "durbdhrbserjvcejg37fg3hcishfjkic"; // key size 256, string length = 256/8 = 32
 
 export default () => {
   U.encryptCompact("hello7!", secretKey).then((x) => {
     console.log(x);
+    const y =
+      "18c9a475d6a8103069466e25e076b424:9e27d4764d01b8f113fd03204069030a"; // should match nexys/crypto - it does not
     U.decryptCompact(x, secretKey).then((e) => console.log(e));
   });
 
