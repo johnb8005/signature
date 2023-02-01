4851c5b13d0c17ec628ded9c04bb3a5989037946
John B <john@nexys.ch>
2023-02-01 01:02:10 +0100

    fix test

--- src/crypto/asymmetric/ecc2.test.ts
+++ src/crypto/asymmetric/ecc2.test.ts
@@ -1,50 +1,63 @@
-import {test, expect, describe} from 'vitest'
+import { test, expect, describe } from "vitest";
 
-import {createKeyPair, deriveKey, encrypt, decrypt, toCipherString, cipherStringToIvAndCipher} from './ecc2'
+import {
+  createKeyPair,
+  deriveKey,
+  encrypt,
+  decrypt,
+  toCipherString,
+  cipherStringToIvAndCipher,
+} from "./ecc2";
 
+describe("create keypairs for Alice and Bob: encrypt and decrypt messages", async () => {
+  const { publicKey: alicePublicKey, privateKey: alicePrivateKey } =
+    await createKeyPair();
+  const { publicKey: bobPublicKey, privateKey: bobPrivateKey } =
+    await createKeyPair();
 
-describe('create keypairs for Alice and Bob: encrypt and decrypt messages', async () => {
-    const { publicKey: alicePublicKey, privateKey: alicePrivateKey} = await createKeyPair()
-    const {publicKey: bobPublicKey, privateKey: bobPrivateKey} = await createKeyPair()
+  const k1 = await deriveKey(alicePrivateKey, bobPublicKey);
+  const k2 = await deriveKey(bobPrivateKey, alicePublicKey);
 
-    const k1 = await deriveKey(alicePrivateKey, bobPublicKey)
-    const k2 = await deriveKey(bobPrivateKey, alicePublicKey)
+  test("derived keys mut be equal", () => {
+    expect(k1).toEqual(k2);
+  });
 
-    test('derived keys mut be equal', () => {
-        expect(k1).toEqual(k2)
-    });
+  const message = "test message2";
+  const iv = crypto.getRandomValues(new Uint8Array(12));
 
-    const message = 'test message2';
-    const iv = crypto.getRandomValues(new Uint8Array(12))
+  const cMessage = new TextEncoder().encode(message);
 
-    const cMessage = new TextEncoder().encode(message)
+  const ctBuffer = await encrypt(cMessage, k1, iv);
+  const cipherString = toCipherString(
+    ctBuffer,
+    iv,
+    "file.txt",
+    "application/json",
+    "mypublickey"
+  );
+  const extracted = cipherStringToIvAndCipher(cipherString);
 
+  test("encrypt", async () => {
+    expect(typeof cipherString).toBe("string");
+  });
 
-    const ctBuffer = await encrypt(cMessage, k1, iv);
-    const cipherString = toCipherString(ctBuffer, iv);
-    const extracted = cipherStringToIvAndCipher(cipherString);
+  test("decrypt", async () => {
+    const decryptedMessage = await decrypt(ctBuffer, k1, iv);
+    expect(new TextDecoder().decode(decryptedMessage)).toBe(message);
+  });
 
-    test('encrypt', async () => {
-       expect(typeof cipherString).toBe('string') 
-    });
+  test("decrypt with k2", async () => {
+    const decryptedMessage = await decrypt(ctBuffer, k2, iv);
+    expect(new TextDecoder().decode(decryptedMessage)).toBe(message);
+  });
 
-    test('decrypt', async () => {
-      const decryptedMessage = await decrypt(ctBuffer, k1, iv);
-      expect(new TextDecoder().decode(decryptedMessage)).toBe(message) 
-    });
+  test("extract iv and cipher", async () => {
+    expect(extracted.ctUint8).toEqual(new Uint8Array(ctBuffer));
+    expect(extracted.iv).toEqual(iv);
+  });
 
-    test('decrypt with k2', async () => {
-        const decryptedMessage = await decrypt(ctBuffer, k2, iv);
-        expect(new TextDecoder().decode(decryptedMessage)).toBe(message) 
-      });
-
-    test('extract iv and cipher', async () => {
-        expect(extracted.ctUint8).toEqual(new Uint8Array(ctBuffer))
-        expect(extracted.iv).toEqual(iv)
-    })
-
-    test('decrypted with extracted iv and cipher', async () => {
-        const decryptedMessage = await decrypt(extracted.ctUint8, k1, extracted.iv)
-        expect(new TextDecoder().decode(decryptedMessage)).toBe(message) 
-    })
-})
+  test("decrypted with extracted iv and cipher", async () => {
+    const decryptedMessage = await decrypt(extracted.ctUint8, k1, extracted.iv);
+    expect(new TextDecoder().decode(decryptedMessage)).toBe(message);
+  });
+});
