716bb2d4c9f2a84783517045d2f31ebf9be8ff43
John B <johan@nexys.ch>
2023-03-02 17:11:04 +0000

    add download on the initiator side

--- src/file-transfer/index.tsx
+++ src/file-transfer/index.tsx
@@ -36,6 +36,11 @@ export default () => {
     return <p>Loading</p>;
   }
 
+  const handleDownloadKeyPair = () => {
+    downloadBlob(publicKey, "public.txt", "text/plain");
+    downloadBlob(privateKey, "private.txt", "text/plain");
+  };
+
   if (state === State.importCipher) {
     const handleUpload = async ({
       target,
@@ -144,6 +149,16 @@ export default () => {
         className="btn btn-outline-primary"
       >
         Import encrypted file <i className="fa fa-arrow-right" />
+      </button>
+
+      <br />
+      <button
+        className="btn btn-outline-secondary"
+        type="button"
+        id="button-addon1"
+        onClick={handleDownloadKeyPair}
+      >
+        <i className="fa fa-key" /> Download Generated Keypair
       </button>
     </>
   );
